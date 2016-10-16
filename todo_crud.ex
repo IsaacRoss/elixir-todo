25acf15c1b642cebe0b5aa9a7aef39856de85e64
Isaac Ross <isaac253@gmail.com>
2016-10-15 23:36:10 -0400

    adding csv import

--- todo_crud.ex
+++ todo_crud.ex
@@ -1,6 +1,15 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: HashDict.new
-  def new, do: %TodoList{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+                entries,
+                %TodoList{},
+                fn(entry, todo_list_acc) -> 
+                  add_entry(todo_list_acc, entry)
+                end
+              )
+  end
 
   def add_entry(
     %TodoList{entries: entries, auto_id: auto_id} = todo_list,
@@ -39,6 +48,54 @@ defmodule TodoList do
         %TodoList{todo_list | entries: new_entries}
     end
   end
+
+  def delete_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id
+  ) do
+    %TodoList{todo_list | entries: HashDict.delete(entries, entry_id)}
+  end 
 end
 
+defmodule TodoList.CsvImporter do
+  def import(file_name) do
+    file_name 
+      |> read_lines
+      |> create_entries
+      |> TodoList.new
+  end
+  
+  defp read_lines(file_name) do
+    file_name 
+      |> File.stream!
+      |> Stream.map(&String.replace(&1, "\n", ""))
+  end
 
+
+  defp create_entries(lines) do
+    lines 
+      |> Stream.map(&extract_fields/1)
+      |> Stream.map(&create_entry/1)
+  end
+
+  defp extract_fields(line) do
+    line
+      |> String.split(",")
+      |> convert_date
+  end
+
+  defp convert_date([date_string, title]) do
+    {parse_date(date_string), title}
+  end
+
+  defp parse_date(date_string) do
+    date_string 
+      |> String.split("/")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple
+  end
+
+  defp create_entry({date, title}) do
+    %{date: date, title: title}
+  end
+end
